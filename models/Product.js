f40321ae0a8dc747017eacf5f950342aa24b8275
abo1fayz <145799866+abo1fayz@users.noreply.github.com>
2025-09-17 22:27:07 +0300

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: String,
+    image: String,
+    
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
